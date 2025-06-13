5e5344694c07f8f2bee1706aa7812969a4a52fba
juandguerrerj <juandguerreroj@gmail.com>
2025-06-13 14:51:38 -0500

    Complete application
    Complete application

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
